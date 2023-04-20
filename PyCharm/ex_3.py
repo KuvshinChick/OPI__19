559a3f96ae88ebf9fbdba504af767bf880eabab6
KuvshinChick <kuvshinirina75@gmail.com>
2023-04-20 22:18:54 +0300

    add PyCharm with examples

---
+++ PyCharm/ex_3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+from datetime import datetime
+
+if __name__ == "__main__":
+    # Найти последний измененный файл
+    time, file_path = max((f.stat().st_mtime, f) for f in pathlib.Path.cwd().iterdir())
+    print(datetime.fromtimestamp(time), file_path)
---
+++ PyCharm/ex_4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+
+
+def unique_path(directory, name_pattern):
+    counter = 0
+    while True:
+        counter += 1
+        path = directory / name_pattern.format(counter)
+        if not path.exists():
+            return path
+
+
+if __name__ == "__main__":
+    # Создать уникальное имя файла
+    # {:03d} - принимает целое число в качестве аргумента и задает минимальную ширину 3
+    # и заполняет оставшиеся места нулями
+    path = unique_path(pathlib.Path.cwd(), 'test{:03d}.txt')
+    print(path)
